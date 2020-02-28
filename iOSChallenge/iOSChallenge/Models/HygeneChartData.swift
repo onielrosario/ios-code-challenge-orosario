6e95f4973aff39502f8e831c87608d6778e37c73
onielrosario <onielrosario@pursuit.org>
2020-02-28 14:11:48 -0700

    create data model and tested endpoint

---
+++ iOSChallenge/iOSChallenge/Models/HygeneChartData.swift
@@ -0,0 +1,15 @@
+//
+//  HygeneChartData.swift
+//  iOSChallenge
+//
+//  Created by Oniel Rosario on 2/28/20.
+//  Copyright © 2020 Oniel Rosario. All rights reserved.
+//
+
+import Foundation
+
+
+struct HygeneChartData: Codable {
+    let t: Int
+    let y: Int
+}
--- iOSChallenge/iOSChallengeTests/iOSChallengeTests.swift
+++ iOSChallenge/iOSChallengeTests/iOSChallengeTests.swift
@@ -10,25 +10,29 @@ import XCTest
 @testable import iOSChallenge
 
 class iOSChallengeTests: XCTestCase {
-
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testChartData() {
+        let endpoint = "https://raw.githubusercontent.com/rune-labs/ios-code-challenge-orosario/master/api/1.json"
+        let exp = expectation(description: "got data")
+        guard let url = URL(string: endpoint) else { return }
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            if let error = error {
+                XCTFail("could not get data: " + error.localizedDescription)
+            } else if let data = data {
+                if let chartData = try? JSONDecoder().decode([HygeneChartData].self, from: data) {
+                    print(chartData.count)
+                    exp.fulfill()
+                }
+            }
+        }.resume()
+        wait(for: [exp], timeout: 5.0)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
